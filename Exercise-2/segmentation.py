86d1a1ca4c97d28db83a9f3a8d0b54c9d9da059c
Pritesh Gudge <priteshgudge@gmail.com>
2017-07-09 12:30:10 -0700

    feat: Finalizing cluster parameters for object rendering

--- Exercise-2/segmentation.py
+++ Exercise-2/segmentation.py
@@ -74,7 +74,7 @@ def cluster_extraction_from_kd_tree(kd_tree, white_cloud):
     ec.set_SearchMethod(kd_tree)
     # Extract indices for each of the discovered clusters
     cluster_indices = ec.Extract()
-    print cluster_indices
+    #print cluster_indices
     return cluster_indices
 
 def generate_cluster_cloud(cluster_indices, white_cloud):
@@ -121,20 +121,49 @@ def pcl_callback(pcl_msg):
 
     extracted_inliers = cloud_filtered.extract(inliers, negative=False)
     extracted_outliers = cloud_filtered.extract(inliers, negative=True)
-
+    
+    cloud_table = extracted_inliers
+    cloud_objects = extracted_outliers
     # TODO: Euclidean Clustering
-    kd_tree, white_cloud = convert_to_kd_tree(extracted_outliers)
-    print kd_tree
-    cluster_indices = cluster_extraction_from_kd_tree(kd_tree, white_cloud)
+    white_cloud = XYZRGB_to_XYZ(cloud_objects)# Apply function to convert XYZRGB to XYZ
+    kd_tree = white_cloud.make_kdtree()
+
+    #Create a cluster extraction object
+    ec = white_cloud.make_EuclideanClusterExtraction()
+    # Set tolerances for distance threshold 
+    # as well as minimum and maximum cluster size (in points)
+    # NOTE: These are poor choices of clustering parameters
+    # Your task is to experiment and find values that work for segmenting objects.
+    ec.set_ClusterTolerance(0.05)
+    ec.set_MinClusterSize(100)
+    ec.set_MaxClusterSize(5000)
+    # Search the k-d tree for clusters
+    ec.set_SearchMethod(kd_tree)
+    # Extract indices for each of the discovered clusters
+    cluster_indices = ec.Extract()
     # TODO: Create Cluster-Mask Point Cloud to visualize each cluster separately
     
-    cluster_cloud = generate_cluster_cloud(cluster_indices, white_cloud)
+    #cluster_cloud = generate_cluster_cloud(cluster_indices, white_cloud)
+    #Assign a color corresponding to each segmented object in scene
+    cluster_color = get_color_list(len(cluster_indices))
+    print cluster_color
+    color_cluster_point_list = []
 
+    for j, indices in enumerate(cluster_indices):
+        for i, indice in enumerate(indices):
+            color_cluster_point_list.append([white_cloud[indice][0],
+                                            white_cloud[indice][1],
+                                            white_cloud[indice][2],
+                                             rgb_to_float(cluster_color[j])])
+
+    #Create new cloud containing all clusters, each with unique color
+    cluster_cloud = pcl.PointCloud_PointXYZRGB()
+    cluster_cloud.from_list(color_cluster_point_list)
 
     # TODO: Convert PCL data to ROS messages
 
-    pcl_obj = pcl_to_ros(extracted_outliers)
-    pcl_table = pcl_to_ros(extracted_inliers)
+    pcl_obj = pcl_to_ros(cloud_objects)
+    pcl_table = pcl_to_ros(cloud_table)
     pcl_cluster_cloud = pcl_to_ros(cluster_cloud)
     #print(pcl_cluster_cloud)
     # TODO: Publish ROS messages
@@ -154,9 +183,10 @@ if __name__ == '__main__':
     pcl_objects_pub = rospy.Publisher("/pcl_objects", PointCloud2, queue_size=1)
 
     pcl_table_pub = rospy.Publisher("/pcl_table", PointCloud2, queue_size=1)
-    # Initialize color_list
 
     pcl_cluster_pub = rospy.Publisher("/pcl_cluster", PointCloud2, queue_size=1)
+
+    # Initialize color_list
     get_color_list.color_list = []
     
     # TODO: Spin while node is not shutdown
